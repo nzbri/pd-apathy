fb5aec0828224e36f9acf07fa46534191b7c3313
Sam Harrison <sam.harrison@nzbri.org>
2021-08-10 10:05:07 +1200

    Reorganise `core_analyses.R`
     + Place data transformations before imputation. MICE is clever enough
       to fill in the conditionals (e.g. `taking_medication` /
       `transformed_dose` appropriately). This makes the transformation code
       simpler, as it is all on a single data frame rather than a `mids`
       object.
     + Place the simplest model first. This gives an intuitive overview and
       is what we're leaning towards.

--- Code/core_analyses.R
+++ Code/core_analyses.R
@@ -84,19 +84,91 @@ full_data %>%
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ###############################################################################
+# Data preprocessing / transformations
+
+# Store means / standard deviations for later
+full_data.mean <- full_data %>%
+  summarise(across(
+    where(is.numeric) | where(is.logical),
+    ~ mean(.x, na.rm = TRUE)
+  ))
+full_data.sd <- full_data %>%
+  summarise(across(
+    where(is.numeric) | where(is.logical),
+    ~ sd(.x, na.rm = TRUE)
+  ))
+
+transformed_data <- full_data %>%
+  mutate(
+    # Add some useful extra timing info
+    years_since_diagnosis = age - age_at_diagnosis,
+    #years_since_symptom_onset = age - age_at_symptom_onset,
+    age = NULL,
+    # Extra confounds
+    session_date2 = scale(session_date) ^ 2,
+    first_session_date2 = scale(first_session_date) ^ 2
+  ) %>%
+  # Center & rescale
+  mutate(across(
+    c(
+      age_at_diagnosis, years_since_diagnosis, education,
+      session_date, session_date2, first_session_date, first_session_date2,
+      MoCA, HADS_depression, HADS_anxiety, UPDRS_motor_score
+    ),
+    ~ as.vector(scale(.x, center = TRUE, scale = TRUE))
+  )) %>%
+  ungroup() %>%
+  # Rescale only
+  #mutate(across(
+  #  c(years_since_diagnosis), #, HADS_depression, HADS_anxiety, UPDRS_motor_score),
+  #  ~ as.vector(scale(.x, center = FALSE, scale = TRUE))
+  #)) %>%
+  #ungroup() %>%
+  # Split LED into two regressors, and rescale
+  mutate(
+    taking_medication = (LED > 0.0),
+    transformed_dose = sqrt(LED),
+    LED = NULL
+  ) %>%
+  group_by(taking_medication) %>%
+  mutate(
+    transformed_dose = as.vector(scale(
+      transformed_dose, center = TRUE, scale = TRUE
+    ))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    transformed_dose = if_else(is.na(transformed_dose), 0.0, transformed_dose)
+  ) %>%
+  #select(taking_medication, transformed_dose) %>% print(n = 100)
+  #ggplot(aes(transformed_dose)) + geom_histogram()
+  # Set reference level for some factors
+  mutate(
+    ethnicity = relevel(ethnicity, ref = "New Zealand European"),
+    taking_medication = factor(taking_medication, levels = c(FALSE, TRUE), labels = c("No", "Yes")),
+    taking_medication = relevel(taking_medication, ref = "Yes"),
+    taking_antidepressants = factor(as.logical(taking_antidepressants), levels = c(FALSE, TRUE), labels = c("No", "Yes")),
+    taking_antidepressants = relevel(taking_antidepressants, ref = "No")
+  )
+
+###############################################################################
 # Imputation
 
-imputed_data <- full_data %>%
+imputed_data <- transformed_data %>%
   mutate(subject_int = as.integer(as.factor(subject_id)))
 
 # Prepare MICE settings
 method <- mice::make.method(imputed_data)
 pred = mice::make.predictorMatrix(imputed_data)  # target = rows, predictors = columns
-# Remove database specific variables
+# Remove database specific variables as predictors
 pred[, c("subject_id", "subject_int", "session_id")] <- 0
 # Remove variables that cause problems as predictors
 # https://stefvanbuuren.name/fimd/sec-toomany.html#finding-problems-loggedevents
 pred[, c("diagnosis", "Hoehn_Yahr", "UPDRS_source")] <- 0
+# No need to impute unused data
+#pred[c(
+#  "age_at_symptom_onset", "WTAR", "NPI_total", "diagnosis", "Hoehn_Yahr", "UPDRS_source"
+#), ] <- 0
 # Tweak default methods for imputing data to account for 2-level structure, where continuous
 # https://stefvanbuuren.name/fimd/sec-mlguidelines.html
 # https://www.gerkovink.com/miceVignettes/Multi_level/Multi_level_data.html
@@ -109,7 +181,7 @@ pred["age_at_symptom_onset", "age_at_diagnosis"] <- 0
 # Run imputation
 imputed_data <- imputed_data %>%
   mice::mice(
-    m = 1, maxit = 10,
+    m = 10, maxit = 10,
     method = method, predictorMatrix = pred
   ) %>%
   # https://stackoverflow.com/a/30892119
@@ -136,69 +208,57 @@ imputed_data <- imputed_data %>%
   mice::as.mids()
 
 ###############################################################################
-# Data preprocessing / transformations
 
-transformed_data <- imputed_data %>%
-  mice::complete(action = "long", include = FALSE) %>%
-  as_tibble() %>%
-  mutate(.id = NULL) %>%
-  # Add some useful extra timing info
-  mutate(years_since_diagnosis = age - age_at_diagnosis)
+full_formula <-
+  NPI_apathy_present ~ 1 +
+  first_session_date + first_session_date2 + session_date + session_date2 +
+  sex + education + age_at_diagnosis +  # ethnicity
+  (1 | subject_id) +
+  years_since_diagnosis + taking_medication + transformed_dose + taking_antidepressants +
+  UPDRS_motor_score + HADS_depression + HADS_anxiety + #global_z  # MoCA
+  attention_domain + executive_domain + language_domain + learning_memory_domain + visuo_domain
 
-# Store means / standard deviations for later
-transformed_data.mean <- transformed_data %>%
-  group_by(.imp) %>%
-  summarise(across(
-    where(is.numeric) | where(is.logical),
-    ~ mean(.x, na.rm = TRUE)
-  ))
-transformed_data.sd <- transformed_data %>%
-  group_by(.imp) %>%
-  summarise(across(
-    where(is.numeric) | where(is.logical),
-    ~ sd(.x, na.rm = TRUE)
-  ))
+prior <- brms::get_prior(
+  formula = full_formula,
+  family = brms::bernoulli(link = "logit"),
+  data = transformed_data
+)
+if (any(prior$class == "b")) {
+  prior <- brms::set_prior("normal(0.0, 1.0)", class = "b")
+} else {
+  prior <- brms::empty_prior()
+}
 
-# Center / rescale selected columns
-transformed_data <- transformed_data %>%
-  mutate(first_session_date2 = scale(first_session_date) ^ 2) %>%
-  # Center & rescale
-  group_by(.imp) %>%
-  mutate(across(
-    c(age_at_diagnosis, education, first_session_date, first_session_date2),
-    ~ as.vector(scale(.x, center = TRUE, scale = TRUE))
-  )) %>%
-  ungroup() %>%
-  # Rescale only
-  group_by(.imp) %>%
-  mutate(across(
-    c(years_since_diagnosis, HADS_depression, UPDRS_motor_score),
-    ~ as.vector(scale(.x, center = FALSE, scale = TRUE))
-  )) %>%
-  ungroup() %>%
-  # Split LED into two regressors, and rescale
-  mutate(
-    taking_medication = (LED > 0.0),
-    transformed_dose = sqrt(LED)
-  ) %>%
-  group_by(.imp, taking_medication) %>%
-  mutate(
-    transformed_dose = as.vector(scale(transformed_dose, center = TRUE, scale = TRUE))
-  ) %>%
-  ungroup() %>%
-  mutate(
-    transformed_dose = if_else(is.na(transformed_dose), 0.0, transformed_dose)
-  ) %>%
-  #select(taking_medication, transformed_dose) %>% print(n = 100)
-  #ggplot(aes(transformed_dose)) + geom_histogram()
-  # Set reference level for some factors
-  mutate(
-    ethnicity = relevel(ethnicity, ref = "New Zealand European"),
-    taking_medication = factor(taking_medication, levels = c(FALSE, TRUE), labels = c("No", "Yes")),
-    taking_medication = relevel(taking_medication, ref = "Yes"),
-    taking_antidepressants = factor(as.logical(taking_antidepressants), levels = c(FALSE, TRUE), labels = c("No", "Yes")),
-    taking_antidepressants = relevel(taking_antidepressants, ref = "No")
-  )
+model <- brms::brm_multiple(
+  formula = full_formula,
+  family = brms::bernoulli(link = "logit"),
+  prior = prior,
+  data = imputed_data,
+  chains = 8, iter = 5000,
+  silent = TRUE, refresh = 0
+)
+print(summary(model))
+
+# Plot odds ratios
+# TODO:
+#  + Rescale by stddev?
+#  + Flip to standard higher is better?
+plt <- fixef(model) %>%
+  exp() %>%  # Odds ratio := exp(beta)
+  as_tibble(rownames = "covariate") %>%
+  filter(covariate != "Intercept") %>%
+  mutate(covariate = factor(covariate, levels = rev(covariate))) %>%
+  ggplot(aes(x = covariate, y = Estimate, ymin = Q2.5, ymax = Q97.5)) +
+  geom_pointrange() +
+  geom_hline(yintercept = 1, linetype = "dashed") +  # add a dotted line at x=1 after flip
+  scale_y_continuous(trans = "log", breaks = c(0.33, 1.0, 3.0)) +
+  coord_flip() +  # flip coordinates (puts labels on y axis)
+  xlab(NULL) +
+  ylab("Odds ratio (95% CI)") +
+  theme_bw()  # use a white background
+
+print(plt)
+#ggsave("test.pdf", plt, width = 6, height = 4, units = "in")
 
 ###############################################################################
 # Fit models
